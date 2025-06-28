19b7c6dfe848d93f397a06061bf28542f70df571
Arnav Panchal <arnavpanchal27@gmail.com>
2025-06-28 11:14:00 +0530

    feat- vercel analytics

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import { useSelector } from "react-redux"
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function HomePage() {
   const { currentUser } = useSelector((state) => state.user)
