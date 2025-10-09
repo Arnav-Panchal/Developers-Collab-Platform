682fde8bdedabf643e7d96092b0f44e198edb75f
Arnav Panchal <arnavpanchal27@gmail.com>
2025-10-09 20:42:28 +0530

    changes

--- api/server.js
+++ api/server.js
@@ -45,7 +45,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
 app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'https://developers-collab-platform.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://www.devcheck.store ');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
